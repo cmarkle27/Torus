701c51efd2b65d0d48be569719f3331ff460028a
Chris Markle <markle976@cdmarkle-2.(none)>
2012-08-05 18:38:05 -0400

    Basic side-to-side movement working.

--- js/tetris.js
+++ js/tetris.js
@@ -1,5 +1,6 @@
 
 // globals
+var boardWidth = 480;
 var piecesContext = document.getElementById("pieces").getContext("2d");
 var tileSize = 40;
 var tileImage = new Image();
@@ -75,28 +76,25 @@ Tetrimino.prototype.getColor = function(shape) {
 	return colors[shape];
 };
 
+// could this use eachBlock???
 Tetrimino.prototype.render = function() {
-	var position = this.position;
-	var depth = this.depth;
-	var color = this.color;
-	var shape = this.getShape(this.shape);
+	var that = this,
+		shape = that.getShape(that.shape),
+		x = shape[that.orientation]["x"],
+		y = shape[that.orientation]["y"],
+		xCoord,
+		yCoord;
 
-	var x = shape[this.orientation]["x"];
-	var y = shape[this.orientation]["y"];
-	var xCoord, yCoord;
-	var ctx = this.context;
-
-
-	ctx.clearRect(0, 0, 480, 600);
+	that.context.clearRect(0, 0, 480, 600);
 
 	for (var i = 0; i < 4; i++) {
-		xCoord = (x[i]*tileSize) + (position*tileSize);
-		yCoord = (y[i]*tileSize) + (depth*tileSize);
-		ctx.beginPath();
-		ctx.rect(xCoord, yCoord, tileSize, tileSize);
-		ctx.fillStyle = color;
-		ctx.fill();
-		ctx.drawImage(tileImage, xCoord, yCoord, tileSize, tileSize);
+		xCoord = (x[i]*tileSize) + (that.position*tileSize);
+		yCoord = (y[i]*tileSize) + (that.depth*tileSize);
+		that.context.beginPath();
+		that.context.rect(xCoord, yCoord, tileSize, tileSize);
+		that.context.fillStyle = that.color;
+		that.context.fill();
+		that.context.drawImage(tileImage, xCoord, yCoord, tileSize, tileSize);
 	}
 
 	// return true or false???
@@ -105,9 +103,114 @@ Tetrimino.prototype.render = function() {
 
 //------------------
 
-var tetrimino = new Tetrimino(piecesContext);
+var GameBoard = function(ctx) {
+	this.context = ctx;
+	this.currentPiece = null;
+};
+
+GameBoard.prototype.createPiece = function() {
+	this.currentPiece = new Tetrimino(piecesContext); // pc is global
+	this.currentPiece.render(); //???
+};
+
+GameBoard.prototype.movePiece = function(direction) {
+
+	var piecePosition = this.currentPiece.position,
+		pieceDepth = this.currentPiece.depth;
+
+	// we need to do a wall check, and a hit check, floor check
+	if (direction === "left") {
+		piecePosition -= 1;
+	} else if (direction === "right") {
+		piecePosition += 1;
+	} else if (direction === "fall") {
+		pieceDepth += 1;
+	}
+
+	if ( this.checkMove(piecePosition, pieceDepth) ) {
+		this.currentPiece.position = piecePosition;
+		this.currentPiece.depth = pieceDepth;
+		this.currentPiece.render();
+	}
+
+};
+
+/*GameBoard.prototype.eachBlock = function(callback) {
+	var that = this,
+		shape = that.getShape(that.shape),
+		x = shape[that.orientation]["x"],
+		y = shape[that.orientation]["y"],
+		xCoord,
+		yCoord;
+
+	for (var i = 0; i < 4; i++) {
+		callback();
+	}
+
+};*/
+
+
+// should this be a local function???
+GameBoard.prototype.checkMove = function(position, depth) {
+	var piece = this.currentPiece,
+		shape = piece.getShape(piece.shape),
+		x = shape[piece.orientation]["x"],
+		y = shape[piece.orientation]["y"],
+		hits = 0,
+		xCoord,
+		yCoord;
+
+	for (i = 0; i < 4; i++) {
+		xCoord = x[i] + position;
+		yCoord = y[i] + depth;
+
+
+		// hit wall
+		if (xCoord === (boardWidth/tileSize) || xCoord < 0) {
+			hits += 1;
+		}
+
+		console.log(xCoord, boardWidth/tileSize, hits);
+		// hit blocks
+		//hits += this.hitCheck(xCoord, yCoord);
+	}
+
+	if (hits > 0) {
+		return false;
+	}
+
+	return true;
+};
+
+
+
+//------------------
+
+
 //var shape = tetrimino.getShape(tetrimino.shape);
 //console.log(tetrimino.shape, " - ", tetrimino.color);
+
+var gameBoard = new GameBoard(); // pass board ctx???
+
+gameBoard.createPiece();
+
+
+//-------------------
+
+$(document).on('keydown', function(e) {
+	switch (e.keyCode) {
+		case 39:
+		case 68:
+			gameBoard.movePiece("right");
+		break;
+
+		case 37:
+		case 65:
+			gameBoard.movePiece("left");
+		break;
+
+	}
+});
 
 
 
